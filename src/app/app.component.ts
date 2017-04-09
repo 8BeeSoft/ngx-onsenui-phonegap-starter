7be94c97d7dbf10b88a8498d8eeda7cf75b66bc4
puku0x <puku0x@gmail.com>
2017-04-10 01:21:20 +0900

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Page1Component } from './page1/page1.component';
 
 @Component({
   selector: 'my-app',
-  template: '<ons-navigator [page]="page"></ons-navigator>',
   styleUrls: ['./app.component.scss'],
+  template: '<ons-navigator [page]="page"></ons-navigator>',
 })
 export class AppComponent {
   page = Page1Component;
