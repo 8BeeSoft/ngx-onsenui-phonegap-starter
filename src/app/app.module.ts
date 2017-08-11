87f4a6e73c3e57d4a6e3a207c0a1ad9e1850aaa7
puku0x <puku0x@gmail.com>
2017-08-12 00:24:09 +0900

    angular 4.3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { OnsenModule } from 'ngx-onsenui';
 
 import { Page1Module } from './page1/page1.module';
@@ -16,7 +16,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
+    HttpClientModule,
     OnsenModule,
     Page1Module,
     Page2Module
